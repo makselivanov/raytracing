45e9541a0cd8c09ff9e2bdacfe21ff40a5b385a5
Makar Selivanov <makar.selivanov@gmail.com>
2024-03-10 15:30:38 +0300

    code style fix

--- src/main/kotlin/raytracing/scene/RenderedScene.kt
+++ src/main/kotlin/raytracing/scene/RenderedScene.kt
@@ -11,9 +11,9 @@ class RenderedScene(val width: Int, val height: Int, val channelSize: Int) {
             val header = "P6\n$width $height\n255\n".toByteArray()
             with (it) {
                 write(header)
-                for (y in 0 until height) {
+                for (y in 0..<height) {
                     val sliceSize = width * channelSize
-                    val slice = image.sliceArray(IntRange(y * sliceSize, (y + 1) * sliceSize))
+                    val slice = image.sliceArray(y * sliceSize ..<(y + 1) * sliceSize)
                     write(slice)
                 }
             }
